e75a4020f5b37624b600283c2ba5d22d22ded32c
Lucy Suddenly <suddenlylucy@gmail.com>
2018-10-16 16:28:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,19 @@ function logWhisper(String) {
 }
 
 function sayHiToGrandma(String) {
+<<<<<<< HEAD
   if (String.toUpperCase() === String) {
     return "YES INDEED!" }
  else if (String.toLowerCase() === String) {
     return "I can\'t hear you!" }
  else if (String === "I love you, Grandma."){
     return "I love you, too." }
+=======
+  String.toUpperCaseCase === String 
+    return "I can't hear you!"
+ String.toLowerCase === String
+    return "YES INDEED!"
+  String === "I love you, Grandma."
+    return "I love you, too."
+>>>>>>> 756f1bb6dc81402440948c09b4be5f397bf9b2c0
 }
